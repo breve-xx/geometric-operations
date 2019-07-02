ee3311299b60b6578facfa893c2fc5361219fa6d
Omar Brescianini <omar.brescianini@gmail.com>
2019-07-02 22:06:04 +0200

    packaging /w dependencies + message.

--- src/main/java/org/altervista/breve/geometricoperations/App.java
+++ src/main/java/org/altervista/breve/geometricoperations/App.java
@@ -2,6 +2,6 @@ package org.altervista.breve.geometricoperations;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("I'm a dummy JAR file, see JUnit for understand me. See You Space Cowboy...");
+        System.out.println("I'm a dummy JAR file, see my JUnit for understand me. See You Space Cowboy...");
     }
 }
